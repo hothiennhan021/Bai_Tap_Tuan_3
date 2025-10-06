12cfcb9e7c26b05d0db4f452748ef94b65e39c2e
hothiennhan021 <hothiennhan021@gmail.com>
2025-10-06 07:08:06 +0700

    Add files via upload

---
+++ DangKy.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Dang_Nhap_Dang_Ky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.images;
+            pictureBox1.Location = new Point(166, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(189, 189);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(278, 266);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(213, 26);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(278, 219);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(213, 26);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(278, 359);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(213, 26);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(278, 314);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(213, 26);
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Times New Roman", 13.8F);
+            label1.ForeColor = Color.DarkSlateBlue;
+            label1.Location = new Point(32, 219);
+            label1.Name = "label1";
+            label1.Size = new Size(158, 26);
+            label1.TabIndex = 5;
+            label1.Text = "Tên Tài Khoản:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Font = new Font("Times New Roman", 13.8F);
+            label2.ForeColor = Color.DarkSlateBlue;
+            label2.Location = new Point(32, 266);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 26);
+            label2.TabIndex = 6;
+            label2.Text = "Mật Khẩu:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.White;
+            label3.Font = new Font("Times New Roman", 13.8F);
+            label3.ForeColor = Color.DarkSlateBlue;
+            label3.Location = new Point(32, 314);
+            label3.Name = "label3";
+            label3.Size = new Size(201, 26);
+            label3.TabIndex = 7;
+            label3.Text = "Xác Nhận Mật Khẩu";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.White;
+            label4.Font = new Font("Times New Roman", 13.8F);
+            label4.ForeColor = Color.DarkSlateBlue;
+            label4.Location = new Point(32, 359);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 26);
+            label4.TabIndex = 8;
+            label4.Text = "Email:";
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.BackColor = Color.Yellow;
+            button1.Font = new Font("Times New Roman", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(199, 412);
+            button1.Name = "button1";
+            button1.Size = new Size(132, 50);
+            button1.TabIndex = 9;
+            button1.Text = "Đăng Ký";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(522, 504);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "DangKy";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+    }
+}

9c8ca8b536fd43a6592d33c0fc303c7ab70d7aaf
phamlau27 <phamlau488@gmail.com>
2025-10-27 21:59:29 +0700

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace AccountUI
+{
+    internal class Connection
+    {
+        private static string StringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Administrator\Downloads\AccountUI\AccountUI\Account.mdf;Integrated Security=True";
+        public static SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection(StringConnection);
+        }
+    }
+}
